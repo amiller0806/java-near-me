84df33d87558b389703cc1956bf5e1e2a30cfd83
Caitlin O'Reilly <coreilly29@gmail.com>
2022-09-11 19:23:19 -0400

    added list items for card that holds concert results including concert name, venue, and date

--- assets/script-concerts.js
+++ assets/script-concerts.js
@@ -2,7 +2,7 @@ var fetchButton = document.getElementById("fetch-button");
 console.log(fetchButton);
 var TmApiKey = "o6fBym3hSy1yQMvpswGVAbQ9Wha16tT8";
 
-// user types in location & getCoffeeApi function is called when the fetchButton is clicked
+// user types in location & getConcerts function is called when the fetchButton is clicked
 function getConcerts(event) {
   event.preventDefault();
   console.log("click");
@@ -35,31 +35,28 @@ function getConcerts(event) {
 
 // display concerts for city
 function displayConcerts(data) {
-  var concertContainer = document.querySelector(".concert-info");
   // for loop to call 10 concerts, and not all 20
   for (let i = 0; i < 10; i++) {
+    var concertContainer = document.querySelector(".concert-info");
     var concertCard = document.createElement("div");
     concertCard.setAttribute("class", "concert-card");
-    var concertItem = document.createElement("div");
-    concertItem.setAttribute("class", "concert-item");
+    var concertList = document.createElement("ol");
+    concertList.className = "concert-list";
+    var concertItem = document.createElement("li");
+    concertItem.className = "concert-item";
     var concertName = document.createElement("p");
     concertName.setAttribute("class", "concertEl");
     var venue = document.createElement("p");
     venue.setAttribute("class", "concertEl");
     var concertDate = document.createElement("p");
-    var concertImg = document.createElement("img");
-    concertImg.setAttribute(
-      "src",
-      "https://app.ticketmaster.com" +
-        data._embedded.events[i].images[0].url +
-        ".png"
-    );
-    console.log(concertImg);
+    // var concertImg = document.createElement("img");
+    // concertImg.setAttribute("src", "concert-image");
     concertDate.setAttribute("class", "concertEl");
     concertName.textContent = `${data._embedded.events[i].name}`;
     venue.textContent = `Venue: ${data._embedded.events[i]._embedded.venues[0].name}`;
     concertDate.textContent = `Date: ${data._embedded.events[i].dates.start.localDate}`;
     concertItem.append(concertName, venue, concertDate);
+    // concertList.append(concertItem);
     concertCard.append(concertItem);
     concertContainer.append(concertCard);
   }
