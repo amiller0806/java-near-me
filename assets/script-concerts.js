16859daa58d7757b65ff2489883cac67b033985b
Caitlin O'Reilly <coreilly29@gmail.com>
2022-09-11 12:37:18 -0400

    working on dynamically creating list of concert results. not finished yet

--- assets/script-concerts.js
+++ assets/script-concerts.js
@@ -3,11 +3,11 @@ console.log(fetchButton);
 var TmApiKey = "xmxhrLJvMZqBKtD916sfNNAvKoMgFHUv";
 
 // user types in location & getCoffeeApi function is called when the fetchButton is clicked
-function getCoffee(event) {
+function getConcerts(event) {
   event.preventDefault();
   console.log("click");
   var city = document.querySelector(".location-search").value;
-  // use Yelp Fusion API key to get list of coffee shops nearby user's location
+  // use Ticket Master API key to get list of concerts nearby user's location
   var requestTicketUrl =
     "https://app.ticketmaster.com/discovery/v2/events.json?segmentId=KZFzniwnSyZfZ7v7nJ&city=" +
     city +
@@ -24,12 +24,26 @@ function getCoffee(event) {
       console.log(
         data._embedded.events[0]._embedded.venues[0].location.latitude
       );
-    });
+      console.log(data._embedded.events[0]._embedded.venues[0].location.longitude);
 
-  /* Method: GET 
-   Authorization: Bearer 
-*/
+    });
 }
 
-// addEventListener activated when user clicks search button, will run the getCoffeeApi function
-fetchButton.addEventListener("click", getCoffee);
+// display concerts for city
+function displayConcerts(data){
+// for loop to call 10 concerts, and not all 20
+  for (let i = 0; i < 10; i++){    
+var concerts = document.createElement("ul");
+concerts.setAttribute("class", "concert-results");
+var name = document.createElement("p");
+name.setAttribute("class", "concertEl");
+name.textContent = `Name: ${data._embedded.events[0].name}`;
+console.log(name);
+concerts.append(name);
+
+
+
+  }
+}
+// addEventListener activated when user clicks search button, will run the getConcerts function
+fetchButton.addEventListener("click", getConcerts);
