d98d79f5fa44081c1ffae44d089032bb16ad1612
Caitlin O'Reilly <coreilly29@gmail.com>
2022-09-11 13:30:01 -0400

    added new ticketmaster api key

--- assets/script-concerts.js
+++ assets/script-concerts.js
@@ -1,6 +1,6 @@
 var fetchButton = document.getElementById("fetch-button");
 console.log(fetchButton);
-var TmApiKey = "xmxhrLJvMZqBKtD916sfNNAvKoMgFHUv";
+var TmApiKey = "o6fBym3hSy1yQMvpswGVAbQ9Wha16tT8";
 
 // user types in location & getCoffeeApi function is called when the fetchButton is clicked
 function getConcerts(event) {
@@ -24,26 +24,27 @@ function getConcerts(event) {
       console.log(
         data._embedded.events[0]._embedded.venues[0].location.latitude
       );
-      console.log(data._embedded.events[0]._embedded.venues[0].location.longitude);
-
+      console.log(
+        data._embedded.events[0]._embedded.venues[0].location.longitude
+      );
+      console.log(data._embedded.events[0].name);
     });
 }
 
 // display concerts for city
-function displayConcerts(data){
-// for loop to call 10 concerts, and not all 20
-  for (let i = 0; i < 10; i++){    
-var concerts = document.createElement("ul");
-concerts.setAttribute("class", "concert-results");
-var name = document.createElement("p");
-name.setAttribute("class", "concertEl");
-name.textContent = `Name: ${data._embedded.events[0].name}`;
-console.log(name);
-concerts.append(name);
-
-
-
-  }
+function displayConcerts(data) {
+  // for loop to call 10 concerts, and not all 20
+  var concertContainer = document.querySelector(".concert-info");
+  var concertCard = document.createElement("div");
+  concertCard.setAttribute("class", "concert-card");
+  var concertItem = document.createElement("div");
+  concertItem.setAttribute("class", "concert-item");
+  var name = document.createElement("p");
+  name.setAttribute("class", "concertEl");
+  name.textContent = data._embedded.events[0].name;
+  concertItem.append(name);
+  concertCard.append(concertItem);
+  concertContainer.append(concertCard);
 }
 // addEventListener activated when user clicks search button, will run the getConcerts function
 fetchButton.addEventListener("click", getConcerts);
